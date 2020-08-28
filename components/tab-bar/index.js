016e16839d3b5255aeda51d2c939a5dda2507c1a
zhougogo <1290612478@qq.com>
2020-08-28 15:41:39 +0800

    xiugai

--- components/tab-bar/index.js
+++ components/tab-bar/index.js
@@ -36,19 +36,19 @@ Component({
       const data = e.currentTarget.dataset
       const url = data.path
       const isInfo = wx.getStorageSync('isInfo')
-      if (url === '/pages/school/index') {
-        console.log(isInfo)
-        if (isInfo) {
-          wx.switchTab({ url })
-        } else {
-          const pages = getCurrentPages()
-          const currentPage = pages[pages.length - 1]
-          if (currentPage.route !== 'pages/user-modify/index') {
-            wx.navigateTo({ url: '/pages/user-modify/index' })
-          }
-        }
-        return
-      }
+      // if (url === '/pages/school/index') {
+      //   console.log(isInfo)
+      //   if (isInfo) {
+      //     wx.switchTab({ url })
+      //   } else {
+      //     const pages = getCurrentPages()
+      //     const currentPage = pages[pages.length - 1]
+      //     if (currentPage.route !== 'pages/user-modify/index') {
+      //       wx.navigateTo({ url: '/pages/user-modify/index' })
+      //     }
+      //   }
+      //   return
+      // }
       wx.switchTab({ url })
     }
   }
